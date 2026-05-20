e7abf915ee1c55238bf95ae21a86170ae5be1692
ahmadyoussefabuhassan <ahmadyoussefabuhassan@gmail.com>
2026-05-20 17:27:32 +0300

    Add RefreshToken Entity

---
+++ E-Learning.Domin/RefreshTokens/IRefreshTokenRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_Learning.Domain.RefreshTokens
+{
+    public interface IRefreshTokenRepository
+    {
+        Task AddSaveToken(RefreshToken token);
+
+        Task<RefreshToken?> GetToken(string token);
+
+        Task DeleteToken(string token);
+    }
+}
---
+++ E-Learning.Domin/RefreshTokens/RefreshToken.cs
@@ -0,0 +1,41 @@
+﻿using E_Learning.Domain.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_Learning.Domain.RefreshTokens
+{
+    public sealed class RefreshToken : Entity
+    {
+        private RefreshToken() : base(Guid.Empty)
+        {
+        }
+        private RefreshToken(Guid Id, string token, string jWTId, DateTime createdAt, DateTime expires, Guid UserId) : base(Id)
+        {
+            Token = token;
+            JWTId = jWTId;
+            CreatedAt = createdAt;
+            Expires = expires;
+            IsUsed = false;
+            IsRevoked = false;
+            this.UserId = UserId;
+        }
+        public string Token { get; private set; }
+        public string JWTId { get; private set; }
+        public DateTime CreatedAt { get; private set; }
+        public DateTime Expires { get; private set; }
+        public bool IsUsed { get; private set; }
+        public bool IsRevoked { get; private set; }
+        public Guid UserId { get; private set; }
+        public bool IsExpired => DateTime.UtcNow >= Expires;
+        public static RefreshToken Create(string token, string jWTId, DateTime createdAt, DateTime expires, Guid userId)
+        {
+            if (string.IsNullOrWhiteSpace(token)) throw new ArgumentException("Token cannot be empty");
+            if (string.IsNullOrWhiteSpace(jWTId)) throw new ArgumentException("JWTId cannot be empty");
+            if (expires <= DateTime.UtcNow) throw new ArgumentException("Expiry date must be in the future");
+            return new RefreshToken(Guid.NewGuid(), token, jWTId, createdAt, expires, userId);
+        }
+    }
+}
