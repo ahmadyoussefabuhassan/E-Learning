645eed51d2a5ea07e5e78e19504b32496614caff
abd.al.sholh@gmail.com <abd.al.sholh@gmail.com>
2026-05-25 01:30:43 +0300

    Errors_abd

---
+++ E-Learning.Domin/InvtensivesVideos/InvtensivesVideosErrors.cs
@@ -0,0 +1,15 @@
+﻿using E_Learning.Domain.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_Learning.Domain.InvtensivesVideos
+{
+    public static class InvtensivesVideosErrors
+    {
+                public static readonly Error NotFound = new(
+                  "InvtensivesVideos.not found" , "لم يتم العثور على فيديو الدورة المكثفة المحدد.");
+    }
+}
---
+++ E-Learning.Domin/Notification/NotificationErrors.cs
@@ -0,0 +1,15 @@
+﻿using E_Learning.Domain.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_Learning.Domain.Notification
+{
+    public static class NotificationErrors
+    {
+        public static readonly Error NotFound = new(
+            "Notification.not found" , "لم يتم العثور على الإشعار المحدد.");
+    }
+}
